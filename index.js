8eca3c2c242c69bb59300995f5f151851987c846
tanastrong <148703474+tanastrong@users.noreply.github.com>
2024-05-11 23:43:50 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
-const myAPIRouter = express.Router()
+const app = express()
+//cors is so we can connect on the front end
+const cors = require('cors')
 
-myAPIRouter.use("/auth", require('./auth'))
-myAPIRouter.use("/products", require('./productRouter'))
-myAPIRouter.use("/reviewers", require('./reviewerRouter'))
-myAPIRouter.use("/product_reviews", require('./product_reviewRouter'))
+const port = process.env.PORT ||8080
+//local file
+const client = require('./db/client')
 
-module.exports = myAPIRouter
+require('dotenv').config()
+app.use(cors())
+app.use(require('morgan')('dev'))
+app.use(express.json())
+
+app.use("/productreviewapi", require('./api/index'))
+   
+const init = async ()=>{
+    await client.connect()
+    app.listen(port, ()=>{
+        console.log("The server is running.")
+    })
+}
+init()
